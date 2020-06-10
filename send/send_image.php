58b98fd8f38e0724a2230e85c36ee8da45393d04
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2020-06-10 18:04:23 +0100

    Initial commit

---
+++ send/send_image.php
@@ -0,0 +1,11 @@
+<?php
+
+global $zoo;
+require_once('../include/zoo.inc');
+$id = (int) get_required_parameter('id');
+$image = $zoo->load('image',$id);
+if (!$image) { exit; }
+header('Content-Type: image/jpeg');
+readfile($image->full_file_name());
+
+?>
