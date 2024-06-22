feeb260ca538b4059961ec5084325fa698d74d6b
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2024-06-22 20:49:04 +0100

    Correct image_id to sound_id

--- ajax/delete_sound.php
+++ ajax/delete_sound.php
@@ -2,7 +2,7 @@
 
 require_once('../include/zoo.inc');
 
-$image_id = (int) get_required_parameter('sound_id');
+$sound_id = (int) get_required_parameter('sound_id');
 $x = $zoo->load('sound',$sound_id);
 if ($x) {
  $x->delete();
