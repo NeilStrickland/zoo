39038a91e487d0174ab13991886b7130b7673c38
Neil Strickland <N.P.Strickland@sheffield.ac.uk>
2025-04-20 17:58:05 +0100

    Add some methods for photos

--- js/zoo.js
+++ js/zoo.js
@@ -1,3 +1,18 @@
-var zoo = Object.create(frog);
-
 zoo.object.ajax_url = '//' + location.hostname + '/zoo/ajax';
+zoo.object.send_url = '//' + location.hostname + '/zoo/send';
+
+zoo.photo.url = function() {
+ return this.send_url + '/send_photo.php?id=' + this.id;
+}
+
+zoo.photo.img = function() {
+ return '<img src="' + this.url() + '"/>';
+}
+
+zoo.photo.small_img = function() {
+ return '<img width="180" src="' + this.url() + '"/>';
+}
+
+zoo.photo.tiny_img = function() {
+ return '<img width="90" src="' + this.url() + '"/>';
+}
